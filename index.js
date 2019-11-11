406c3014dd7f44a5b5c5d43dfe6d283ff0f33660
Jean-Philippe Bidegain <jp@bidega.in>
2019-11-11 11:46:18 +0100

    Bump version 0.0.5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const VERSION = '0.0.4';
+const VERSION = '0.0.5';
 const SoulseekCli = require('./src/soulseek-cli');
 
 program.version(VERSION);
