3277e2215fb3306d8229661b3aa828afbcfe4701
Jean-Philippe Bidegain <jp@bidega.in>
2019-11-14 07:18:02 +0100

    Fix indentation

--- src/modules/DestinationDirectory.js
+++ src/modules/DestinationDirectory.js
@@ -3,27 +3,27 @@ const fs = require('fs');
 const process = require('process');
 
 module.exports = function(destination) {
-    this.destination = destination;
+  this.destination = destination;
 
-    /**
-     * Compute the final destination repository, depending on the "destination" option
-     * @param  {string} directory
-     * @return {string}
-     */
-    this.getDestinationDirectory = (directory) => {
-      let dir;
-      if (this.destination) {
-        if (path.isAbsolute(this.destination)) {
-          dir = this.destination  + '/' + directory;
-        } else {
-          dir = process.cwd() + '/' + this.destination   + '/' + directory;
-        }
+  /**
+   * Compute the final destination repository, depending on the "destination" option
+   * @param  {string} directory
+   * @return {string}
+   */
+  this.getDestinationDirectory = (directory) => {
+    let dir;
+    if (this.destination) {
+      if (path.isAbsolute(this.destination)) {
+        dir = this.destination  + '/' + directory;
       } else {
-        dir = process.cwd() + '/' + directory;
+        dir = process.cwd() + '/' + this.destination   + '/' + directory;
       }
-      createIfNotExist(dir)
-      return dir;
+    } else {
+      dir = process.cwd() + '/' + directory;
     }
+    createIfNotExist(dir)
+    return dir;
+  }
 }
 
 let createIfNotExist = (path) => {
