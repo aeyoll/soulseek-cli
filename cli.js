5bad968f866461638ee00c474a8fb7293c8c3c63
Jean-Philippe Bidegain <jp@bidega.in>
2020-07-16 06:52:58 +0200

    Bump version

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const program = require('commander');
-const VERSION = '0.0.13';
+const VERSION = '0.0.14';
 const SoulseekCli = require('./src/commands/soulseek-cli');
 const Login = require('./src/commands/login');
 
