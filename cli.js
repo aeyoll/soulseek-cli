8ea10b06f4f96755fb4a0fb15066e2454d4f3a18
Jean-Philippe Bidegain <jp@bidega.in>
2021-02-20 18:37:33 +0100

    Fix the default value in the help text for the mode option

--- cli.js
+++ cli.js
@@ -13,7 +13,7 @@ program
   .description('Download with required query')
   .option('-d, --destination <folder>', 'downloads\'s destination')
   .option('-q, --quality <quality>', 'show only mp3 with a defined quality')
-  .option('-m, --mode <mode>', 'filter the kind of files you want (available: "mp3", "flac", default: "flac")', 'mp3')
+  .option('-m, --mode <mode>', 'filter the kind of files you want (available: "mp3", "flac", default: "mp3")', 'mp3')
   .alias('d')
   .action((queries, options) => {
     new DownloadCommand(queries, options);
@@ -23,7 +23,7 @@ program
   .command('query [query...]')
   .description('Search with required query, but don\'t download anything')
   .option('-q, --quality <quality>', 'show only mp3 with a defined quality')
-  .option('-m, --mode <mode>', 'filter the kind of files you want (available: "mp3", "flac", default: "flac")', 'mp3')
+  .option('-m, --mode <mode>', 'filter the kind of files you want (available: "mp3", "flac", default: "mp3")', 'mp3')
   .alias('q')
   .action((queries, options) => {
     new QueryCommand(queries, options);
